b25c63e8d3c875fae7bfc9e15bd5d95524b64cbc
Feraris20 <ferhati.aris@gmail.com>
2025-09-03 11:09:36 +0300

    First update
    This is first simple update to check what happens

--- Start.ps1
+++ Start.ps1
@@ -1,1 +1,12 @@
-msg * /time:15 "✅ .NET Framework 3.5 installed successfully."
+Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -All
+# msg * /time:15 "✅ .NET Framework 3.5 installed successfully."
+Write-Output "*****************************************"
+Write-Output "Winget version: $(winget --version)"
+winget install --id=Microsoft.AppInstaller --accept-source-agreements --accept-package-agreements --silent
+Write-Output "*****************************************"
+Write-Output "Winget version: $(winget --version)"
+
+
+$apps = @("Microsoft.VisualStudioCode", "abbodi1406.vcredist", "M2Team.NanaZip", "Nilesoft.Shell")
+foreach ($app in $apps) { winget install --id=$app --accept-source-agreements --accept-package-agreements --silent }
+Write-Output "Winget version: $(winget --version)"
