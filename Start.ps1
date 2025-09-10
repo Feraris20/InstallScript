d92b4033e4e7d5e5520230ea4a0b59d301e5cc66
Feraris20 <ferhati.aris@gmail.com>
2025-09-10 16:21:35 +0300

    Add apps

--- Start.ps1
+++ Start.ps1
@@ -31,7 +31,7 @@ $cbInstallApps.Checked = $true
 $checkboxes += $cbInstallApps
 
 $cbWindowsDefender = New-Object System.Windows.Forms.CheckBox
-$cbWindowsDefender.Text = "Disable Windows Defender (zoicware DefenderProTools)"
+$cbWindowsDefender.Text = "Disable Windows Defender (kutt.it/off)" #(zoicware DefenderProTools)"
 $cbWindowsDefender.Location = New-Object System.Drawing.Point(20,110)
 $cbWindowsDefender.AutoSize = $true
 $cbWindowsDefender.Checked = $true
@@ -127,7 +127,7 @@ function proceedWithActions {
     if ($cbInstallApps.Checked) {
         $scriptBlocks += {
             try {
-                $apps = @("Microsoft.VisualStudioCode", "abbodi1406.vcredist", "M2Team.NanaZip", "Nilesoft.Shell")
+                $apps = @("Microsoft.VisualStudioCode", "abbodi1406.vcredist", "M2Team.NanaZip", "IrfanSkiljan.IrfanView", "IrfanSkiljan.IrfanView.PlugIns", "Nilesoft.Shell")
                 foreach ($app in $apps) {
                     Write-Host "Installing $app ..."
                     winget install --id=$app --accept-source-agreements --accept-package-agreements --silent
