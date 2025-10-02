d59b11bc2b46dd27da2236a0bcb48304f3812c98
Feraris20 <ferhati.aris@gmail.com>
2025-10-02 17:16:01 +0300

    Fixed extras.

--- Main.ps1
+++ Main.ps1
@@ -3,14 +3,14 @@ Add-Type -AssemblyName System.Windows.Forms
 # Create the form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Main Installer"
-$form.Size = New-Object System.Drawing.Size(520,400)
+$form.Size = New-Object System.Drawing.Size(520, 400)
 $form.StartPosition = "CenterScreen"
 
 # Checkbox for .NET Framework 3.5
 $checkboxNetFx = New-Object System.Windows.Forms.CheckBox
 $checkboxNetFx.Text = "Enable .NET Framework 3.5"
 $checkboxNetFx.AutoSize = $true
-$checkboxNetFx.Location = New-Object System.Drawing.Point(20,20)
+$checkboxNetFx.Location = New-Object System.Drawing.Point(20, 20)
 $form.Controls.Add($checkboxNetFx)
 
 # Checkbox for Microsoft App Installer
@@ -18,7 +18,7 @@ $checkboxAppInstaller = New-Object System.Windows.Forms.CheckBox
 $checkboxAppInstaller.Text = "Install Microsoft App Installer"
 $checkboxAppInstaller.AutoSize = $true
 $checkboxAppInstaller.Checked = $true
-$checkboxAppInstaller.Location = New-Object System.Drawing.Point(20,50)
+$checkboxAppInstaller.Location = New-Object System.Drawing.Point(20, 50)
 $form.Controls.Add($checkboxAppInstaller)
 
 # Checkbox for installing additional apps
@@ -26,7 +26,7 @@ $checkboxApps = New-Object System.Windows.Forms.CheckBox
 $checkboxApps.Text = "Install Additional Apps"
 $checkboxApps.AutoSize = $true
 $checkboxApps.Checked = $true
-$checkboxApps.Location = New-Object System.Drawing.Point(20,80)
+$checkboxApps.Location = New-Object System.Drawing.Point(20, 80)
 $form.Controls.Add($checkboxApps)
 
 # Checkbox for "Install Floorp + Extras" (checked by default)
@@ -34,7 +34,7 @@ $checkboxFloorpExtras = New-Object System.Windows.Forms.CheckBox
 $checkboxFloorpExtras.Text = "Install Floorp + Extras"
 $checkboxFloorpExtras.AutoSize = $true
 $checkboxFloorpExtras.Checked = $true
-$checkboxFloorpExtras.Location = New-Object System.Drawing.Point(20,110)
+$checkboxFloorpExtras.Location = New-Object System.Drawing.Point(20, 110)
 $form.Controls.Add($checkboxFloorpExtras)
 
 # Checkbox for Disable Services
@@ -42,14 +42,14 @@ $checkboxDisableServices = New-Object System.Windows.Forms.CheckBox
 $checkboxDisableServices.Text = "Disable Selected Services"
 $checkboxDisableServices.AutoSize = $true
 $checkboxDisableServices.Checked = $true
-$checkboxDisableServices.Location = New-Object System.Drawing.Point(20,140)
+$checkboxDisableServices.Location = New-Object System.Drawing.Point(20, 140)
 $form.Controls.Add($checkboxDisableServices)
 
 # Apply Button
 $button = New-Object System.Windows.Forms.Button
 $button.Text = "Apply"
-$button.Size = New-Object System.Drawing.Size(80,30)
-$button.Location = New-Object System.Drawing.Point(420,350)
+$button.Size = New-Object System.Drawing.Size(80, 30)
+$button.Location = New-Object System.Drawing.Point(420, 350)
 $form.Controls.Add($button)
 
 
@@ -63,14 +63,16 @@ function Install-FloorpExtras {
     $registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\$ExecutableName"
 
     try {
+        # Check if registry key exists
         if (-not (Test-Path $registryPath)) {
             Write-Output "Registry key for $ExecutableName not found."
             return
         }
 
+        # Get the default value which typically holds the program path
         $programPath = (Get-ItemProperty -Path $registryPath -ErrorAction Stop).'(Default)'
 
-        if ($programPath) {
+        if ($programPath -and (Test-Path $programPath)) {
             $installDirectory = Split-Path -Path $programPath -Parent
 
             # Create 'distribution' folder if needed
@@ -78,7 +80,8 @@ function Install-FloorpExtras {
             if (-not (Test-Path $distributionFolder)) {
                 New-Item -Path $distributionFolder -ItemType Directory | Out-Null
                 Write-Output "Created folder: $distributionFolder"
-            } else {
+            }
+            else {
                 Write-Output "Folder already exists: $distributionFolder"
             }
 
@@ -100,12 +103,16 @@ function Install-FloorpExtras {
 }
 "@
 
-            [System.IO.File]::WriteAllText($policiesFilePath, $jsonContent, New-Object System.Text.UTF8Encoding($false))
+            # Write JSON to file without BOM
+            $utf8NoBom = New-Object System.Text.UTF8Encoding($false)
+            [System.IO.File]::WriteAllText($policiesFilePath, $jsonContent, $utf8NoBom)
             Write-Output "Created policies.json at: $policiesFilePath"
-        } else {
+        }
+        else {
             Write-Output "Program path not found in registry for $ExecutableName."
         }
-    } catch {
+    }
+    catch {
         Write-Output "Error during Floorp Extras setup: $($_.Exception.Message)"
     }
 }
@@ -114,47 +121,47 @@ function Install-FloorpExtras {
 function Configure-Services {
     # List of services to disable
     $servicesToDisable = @(
-        "AJRouter","AppVClient","Appinfo","AssignedAccessManagerSvc","BDESVC",
-        "BTAGService","BcastDVRUserService","BluetoothUserService","DiagTrack",
-        "DialogBlockingService","DevQueryBroker","DeviceAssociationService",
-        "DeviceInstall","DevicePickerUserSvc","DevicesFlowUserSvc","RemoteAccess",
-        "RemoteRegistry","SensorDataService","SensrSvc","SharedAccess",
-        "SmsRouter","StiSvc","StateRepository","StorSvc","TapiSrv",
-        "TextInputManagementService","TieringEngineService","TokenBroker",
-        "TroubleshootingSvc","UdkUserSvc","UnistoreSvc","WpnService","WwanSvc",
-        "autotimesvc","bthserv","cbdhsvc","cloudidsvc","dcsvc","defragsvc",
-        "diagnosticshub.standardcollector.service","diagsvc","dmwappushservice",
-        "dot3svc","embeddedmode","fdPHost","fhsvc","hidserv","icssvc",
-        "lfsvc","lltdsvc","lmhosts","netprofm","p2pimsvc","p2psvc",
-        "perceptionsimulation","seclogon","smphost","svsvc","swprv",
-        "tzautoupdate","upnphost","vds","vmicguestinterface","vmicheartbeat",
-        "vmickvpexchange","vmicrdv","vmicshutdown","vmictimesync","vmicvmsession",
-        "vmicvss","vmvss","wbengine","wcncsvc","webthreatdefsvc","wercplsupport",
-        "wisvc","wlidsvc","wlpasvc","wmiApSrv","workfolderssvc","wuauserv",
+        "AJRouter", "AppVClient", "Appinfo", "AssignedAccessManagerSvc", "BDESVC",
+        "BTAGService", "BcastDVRUserService", "BluetoothUserService", "DiagTrack",
+        "DialogBlockingService", "DevQueryBroker", "DeviceAssociationService",
+        "DeviceInstall", "DevicePickerUserSvc", "DevicesFlowUserSvc", "RemoteAccess",
+        "RemoteRegistry", "SensorDataService", "SensrSvc", "SharedAccess",
+        "SmsRouter", "StiSvc", "StateRepository", "StorSvc", "TapiSrv",
+        "TextInputManagementService", "TieringEngineService", "TokenBroker",
+        "TroubleshootingSvc", "UdkUserSvc", "UnistoreSvc", "WpnService", "WwanSvc",
+        "autotimesvc", "bthserv", "cbdhsvc", "cloudidsvc", "dcsvc", "defragsvc",
+        "diagnosticshub.standardcollector.service", "diagsvc", "dmwappushservice",
+        "dot3svc", "embeddedmode", "fdPHost", "fhsvc", "hidserv", "icssvc",
+        "lfsvc", "lltdsvc", "lmhosts", "netprofm", "p2pimsvc", "p2psvc",
+        "perceptionsimulation", "seclogon", "smphost", "svsvc", "swprv",
+        "tzautoupdate", "upnphost", "vds", "vmicguestinterface", "vmicheartbeat",
+        "vmickvpexchange", "vmicrdv", "vmicshutdown", "vmictimesync", "vmicvmsession",
+        "vmicvss", "vmvss", "wbengine", "wcncsvc", "webthreatdefsvc", "wercplsupport",
+        "wisvc", "wlidsvc", "wlpasvc", "wmiApSrv", "workfolderssvc", "wuauserv",
         "wudfsvc"
     )
 
     # List of services to set to demand
     $servicesToDemand = @(
-        "ALG","AppIDSvc","AppMgmt","AppReadiness","AppXSvc","CertPropSvc","ClipSVC",
-        "ConsentUxUserSvc","CscService","DeviceInstall","DevicePickerUserSvc",
-        "DevicesFlowUserSvc","DisplayEnhancementService","DmEnrollmentSvc","DsSvc",
-        "DsmSvc","EFS","EapHost","EntAppSvc","FDResPub","FrameServer",
-        "FrameServerMonitor","GraphicsPerfSvc","HvHost","IEEtwCollectorService",
-        "InstallService","InventorySvc","IpxlatCfgSvc","KtmRm","LicenseManager",
-        "LxpSvc","MSDTC","MSiSCSI","McpManagementService","MessagingService",
-        "MicrosoftEdgeElevationService","MsKeyboardFilter","NPSMSvc",
-        "NaturalAuthentication","NcaSvc","NcbService","NcdAutoSetup",
-        "NetSetupSvc","NetTcpPortSharing","Netman","NgcCtnrSvc","NgcSvc",
-        "NlaSvc","P9RdrService","PNRPAutoReg","PNRPsvc","PcaSvc","PeerDistSvc",
-        "PenService","PerfHost","PhoneSvc","PimIndexMaintenanceSvc","PlugPlay",
-        "PolicyAgent","PrintNotify","PushToInstall","QWAVE","RasAuto","RasMan",
-        "SCPolicySvc","SCardSvr","SDRSVC","SEMgrSvc","SNMPTRAP","SNMPTrap",
-        "SSDPSRV","ScDeviceEnum","SensorService","SessionEnv","SmsRouter",
-        "SstpSvc","StiSvc","WerSvc","WiaRpc","WinHttpAutoProxySvc","WinRM",
-        "WpcMonSvc","WdiServiceHost","WdiSystemHost","WebClient",
-        "Wecsvc","Wersvc","WiaRpc","WinHttpAutoProxySvc","WinRM","WpcMonSvc",
-        "WdiServiceHost","WdiSystemHost","webthreatdefsvc","wermgr","WbioSrvc"
+        "ALG", "AppIDSvc", "AppMgmt", "AppReadiness", "AppXSvc", "CertPropSvc", "ClipSVC",
+        "ConsentUxUserSvc", "CscService", "DeviceInstall", "DevicePickerUserSvc",
+        "DevicesFlowUserSvc", "DisplayEnhancementService", "DmEnrollmentSvc", "DsSvc",
+        "DsmSvc", "EFS", "EapHost", "EntAppSvc", "FDResPub", "FrameServer",
+        "FrameServerMonitor", "GraphicsPerfSvc", "HvHost", "IEEtwCollectorService",
+        "InstallService", "InventorySvc", "IpxlatCfgSvc", "KtmRm", "LicenseManager",
+        "LxpSvc", "MSDTC", "MSiSCSI", "McpManagementService", "MessagingService",
+        "MicrosoftEdgeElevationService", "MsKeyboardFilter", "NPSMSvc",
+        "NaturalAuthentication", "NcaSvc", "NcbService", "NcdAutoSetup",
+        "NetSetupSvc", "NetTcpPortSharing", "Netman", "NgcCtnrSvc", "NgcSvc",
+        "NlaSvc", "P9RdrService", "PNRPAutoReg", "PNRPsvc", "PcaSvc", "PeerDistSvc",
+        "PenService", "PerfHost", "PhoneSvc", "PimIndexMaintenanceSvc", "PlugPlay",
+        "PolicyAgent", "PrintNotify", "PushToInstall", "QWAVE", "RasAuto", "RasMan",
+        "SCPolicySvc", "SCardSvr", "SDRSVC", "SEMgrSvc", "SNMPTRAP", "SNMPTrap",
+        "SSDPSRV", "ScDeviceEnum", "SensorService", "SessionEnv", "SmsRouter",
+        "SstpSvc", "StiSvc", "WerSvc", "WiaRpc", "WinHttpAutoProxySvc", "WinRM",
+        "WpcMonSvc", "WdiServiceHost", "WdiSystemHost", "WebClient",
+        "Wecsvc", "Wersvc", "WiaRpc", "WinHttpAutoProxySvc", "WinRM", "WpcMonSvc",
+        "WdiServiceHost", "WdiSystemHost", "webthreatdefsvc", "wermgr", "WbioSrvc"
     )
 
     # Disable services loop with output
@@ -165,13 +172,16 @@ function Configure-Services {
                 $result = $svc.ChangeStartMode("Disabled")
                 if ($result.ReturnValue -eq 0) {
                     Write-Host "Disabled service: $service" -ForegroundColor Gray
-                } else {
+                }
+                else {
                     Write-Host "Failed to disable $service (ReturnCode: $($result.ReturnValue))" -ForegroundColor Red
                 }
-            } else {
+            }
+            else {
                 Write-Host "Service not found: $service" -ForegroundColor Yellow
             }
-        } catch {
+        }
+        catch {
             Write-Host "Error disabling $service : $_" -ForegroundColor Red
         }
     }
@@ -184,13 +194,16 @@ function Configure-Services {
                 $result = $svc.ChangeStartMode("Manual")
                 if ($result.ReturnValue -eq 0) {
                     Write-Host "Set service to demand: $service" -ForegroundColor Gray
-                } else {
+                }
+                else {
                     Write-Host "Failed to set $service to demand (ReturnCode: $($result.ReturnValue))" -ForegroundColor Red
                 }
-            } else {
+            }
+            else {
                 Write-Host "Service not found: $service" -ForegroundColor Yellow
             }
-        } catch {
+        }
+        catch {
             Write-Host "Error setting $service to demand: $_" -ForegroundColor Red
         }
     }
@@ -200,71 +213,74 @@ function Configure-Services {
 
 # Your Apply button click event
 $button.Add_Click({
-    # Clear the console
-    Clear-Host
-    Write-Host "Starting operations..." -ForegroundColor Cyan
+        # Clear the console
+        Clear-Host
+        Write-Host "Starting operations..." -ForegroundColor Cyan
 
-    # 1. Enable .NET Framework 3.5
-    if ($checkboxNetFx.Checked) {
-        Write-Host "Enabling .NET Framework 3.5..." -ForegroundColor Yellow
-        try {
-            Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -All -NoRestart -ErrorAction Stop
-            Write-Host ".NET Framework 3.5 enabled successfully." -ForegroundColor Green
-        } catch {
-            Write-Host "Error enabling .NET Framework 3.5: $($_.Exception.Message)" -ForegroundColor Red
-        }
-    }
-
-    # 2. Install Microsoft App Installer
-    if ($checkboxAppInstaller.Checked) {
-        Write-Host "Installing Microsoft App Installer..." -ForegroundColor Yellow
-        try {
-            winget install --id=Microsoft.AppInstaller --accept-source-agreements --accept-package-agreements
-            Write-Host "Microsoft App Installer installed successfully." -ForegroundColor Green
-        } catch {
-            Write-Host "Error installing Microsoft App Installer: $($_.Exception.Message)" -ForegroundColor Red
-        }
-    }
-
-    # 3. Install additional apps
-    if ($checkboxApps.Checked) {
-        $apps = @(
-            "Microsoft.VisualStudioCode",
-            "abbodi1406.vcredist",
-            "M2Team.NanaZip",
-            "IrfanSkiljan.IrfanView",
-            "IrfanSkiljan.IrfanView.PlugIns",
-            "CodecGuide.K-LiteCodecPack.Standard",
-            "Ablaze.Floorp",
-            "Nilesoft.Shell"
-        )
-        foreach ($app in $apps) {
-            Write-Host "Installing $app ..." -ForegroundColor Cyan
+        # 1. Enable .NET Framework 3.5
+        if ($checkboxNetFx.Checked) {
+            Write-Host "Enabling .NET Framework 3.5..." -ForegroundColor Yellow
             try {
-                winget install --id=$app --accept-source-agreements --accept-package-agreements --silent
-                Write-Host "$app installed successfully." -ForegroundColor Green
-            } catch {
-                Write-Host "Error installing $($app): ${($_.Exception.Message)}" -ForegroundColor Red
+                Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -All -NoRestart -ErrorAction Stop
+                Write-Host ".NET Framework 3.5 enabled successfully." -ForegroundColor Green
             }
-            Start-Sleep -Seconds 1
+            catch {
+                Write-Host "Error enabling .NET Framework 3.5: $($_.Exception.Message)" -ForegroundColor Red
+            }
         }
-        Write-Host "All selected apps installed." -ForegroundColor Cyan
-    }
 
-    # 4. Install Floorp + Extras if checked
-    if ($checkboxFloorpExtras.Checked) {
-        Write-Host "Installing Floorp + Extras..." -ForegroundColor Yellow
-        Install-FloorpExtras
-    }
+        # 2. Install Microsoft App Installer
+        if ($checkboxAppInstaller.Checked) {
+            Write-Host "Installing Microsoft App Installer..." -ForegroundColor Yellow
+            try {
+                winget install --id=Microsoft.AppInstaller --accept-source-agreements --accept-package-agreements
+                Write-Host "Microsoft App Installer installed successfully." -ForegroundColor Green
+            }
+            catch {
+                Write-Host "Error installing Microsoft App Installer: $($_.Exception.Message)" -ForegroundColor Red
+            }
+        }
 
-    # 5. Configure services if checkbox checked
-    if ($checkboxDisableServices.Checked) {
-        Write-Host "Configuring services (disable then demand)..." -ForegroundColor Yellow
-        Configure-Services
-    }
+        # 3. Install additional apps
+        if ($checkboxApps.Checked) {
+            $apps = @(
+                "Microsoft.VisualStudioCode",
+                "abbodi1406.vcredist",
+                "M2Team.NanaZip",
+                "IrfanSkiljan.IrfanView",
+                "IrfanSkiljan.IrfanView.PlugIns",
+                "CodecGuide.K-LiteCodecPack.Standard",
+                "Ablaze.Floorp",
+                "Nilesoft.Shell"
+            )
+            foreach ($app in $apps) {
+                Write-Host "Installing $app ..." -ForegroundColor Cyan
+                try {
+                    winget install --id=$app --accept-source-agreements --accept-package-agreements --silent
+                    Write-Host "$app installed successfully." -ForegroundColor Green
+                }
+                catch {
+                    Write-Host "Error installing $($app): ${($_.Exception.Message)}" -ForegroundColor Red
+                }
+                Start-Sleep -Seconds 1
+            }
+            Write-Host "All selected apps installed." -ForegroundColor Cyan
+        }
 
-    Write-Host "Operations completed." -ForegroundColor Cyan
-})
+        # 4. Install Floorp + Extras if checked
+        if ($checkboxFloorpExtras.Checked) {
+            Write-Host "Installing Floorp + Extras..." -ForegroundColor Yellow
+            Install-FloorpExtras
+        }
+
+        # 5. Configure services if checkbox checked
+        if ($checkboxDisableServices.Checked) {
+            Write-Host "Configuring services (disable then demand)..." -ForegroundColor Yellow
+            Configure-Services
+        }
+
+        Write-Host "Operations completed." -ForegroundColor Cyan
+    })
 
 # Show the form
 $form.Show()
