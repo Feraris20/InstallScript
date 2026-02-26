27edbf2393faa60019a2327dd769c5e2ae0d7ffe
Feraris20 <ferhati.aris@gmail.com>
2026-02-26 14:57:27 +0200

    Fix Startup

--- Start.ps1
+++ Start.ps1
@@ -1,24 +1,21 @@
-if ($env:IS_RELAUNCH -ne "1") {
-    $env:IS_RELAUNCH = "1"
 
-    $script = if ($PSCommandPath) {
-        "& { & `'$($PSCommandPath)`' $($argList -join ' ') }"
-    }
-    else {
-        "&([ScriptBlock]::Create((irm https://raw.githubusercontent.com/Feraris20/InstallScript/refs/heads/main/Main.ps1))) $($argList -join ' ')"
-    }
 
-    $powershellCmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
-    $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { "$powershellCmd" }
-
-    if ($processCmd -eq "wt.exe") {
-        Start-Process $processCmd -ArgumentList "$powershellCmd -ExecutionPolicy Bypass -NoProfile -Command `"$script`"" -Verb RunAs
-    }
-    else {
-        Start-Process $processCmd -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command `"$script`"" -Verb RunAs
-    }
-    $env:IS_RELAUNCH = "0"
-    exit
+$script = if ($PSCommandPath) {
+    "& { & `'$($PSCommandPath)`' $($argList -join ' ') }"
 }
+else {
+    "&([ScriptBlock]::Create((irm https://raw.githubusercontent.com/Feraris20/InstallScript/refs/heads/main/Main.ps1))) $($argList -join ' ')"
+}
+
+$powershellCmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
+$processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { "$powershellCmd" }
+
+if ($processCmd -eq "wt.exe") {
+    Start-Process $processCmd -ArgumentList "$powershellCmd -ExecutionPolicy Bypass -NoProfile -Command `"$script`"" -Verb RunAs
+}
+else {
+    Start-Process $processCmd -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command `"$script`"" -Verb RunAs
+}
+
 
 exit
